37965aa6bf7c41e4746a24549d07cd254671116b
izzatillonemataliev <156827918+izzatillonemataliev@users.noreply.github.com>
2024-05-30 15:59:50 +0500

    гщв

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+// style
+import './footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className="footer-container">
+        <p>All rights reserved.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
